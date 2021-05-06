f18c3095766085f5c44c56d63236b4342bb191b6
Maciej <maciej.szulgacz@gmail.com>
2021-05-06 19:28:56 +0200

    Models

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    position = models.CharField(max_length=64)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=128)
+    start_date = models.DateField()
+    finish_date = models.DateField(blank=True)
+    person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
+
+
+class Machine(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+    task = models.ForeignKey(Task, on_delete=models.SET_NULL, null=True)
+
+
+class Subcontractor(models.Model):
+    INDUSTRY_CHOICES = (
+        (1, 'Construction Works'),
+        (2, 'Bridge Works'),
+        (3, 'Road Works'),
+        (4, 'Track Works'),
+        (5, 'Sanitary Works'),
+        (6, 'Telecommunication Works'),
+        (7, 'Electric Works'),
+    )
+    name = models.CharField(max_length=64, unique=True)
+    industry = models.IntegerField(choices=INDUSTRY_CHOICES)
+    tasks = models.ManyToManyField(Task)
+
+
+class ConstructionSide(models.Model):
+    name = models.CharField(max_length=64, unique=True)
