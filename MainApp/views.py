c6f5ad57355759f0d313ebf7bb2e2c698baba2c1
Maciej <maciej.szulgacz@gmail.com>
2021-05-07 19:00:06 +0200

    BaseView

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Task
 
-# Create your views here.
+
+class BaseView(View):
+    template_name = "MainApp/Base_form.html"
+
+    def get(self, request, *args, **kwargs):
+        tasks = Task.objects.all()
+        return render(request, self.template_name, {"tasks": tasks})
