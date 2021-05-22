60c31f4ee12f5336473abc14365e68177f77fa31
Maciej <maciej.szulgacz@gmail.com>
2021-05-22 08:05:33 +0200

    test_add_machine test

--- MainApp/tests.py
+++ MainApp/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import pytest
+from .models import Machine
 
-# Create your tests here.
+# @pytest.mark.django_db
+# def test_display_machine_detail_site(task, client):
+#     pk = task.pk
+#     response = client.get(f'/task-details/{pk}')
+#     assert response.status_code == 200
+#     assert 'name' in response.context
+#     assert response.context['name'] == task.name
+
+
+@pytest.mark.django_db
+def test_add_machine(client):
+    response = client.post('/add-machine/',
+                           {'machine_model': 'Excavator',
+                            'description': 'Machine for excavation',
+                            'price': 5000})
+    assert Machine.objects.get(machine_model='Excavator')
+    assert response.status_code == 302
+
+
+
